019da0d5a38530d2134143dec88ef44aef9b564a
Dani <enderodri@gmail.com>
2021-05-10 17:08:09 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,6 +58,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             
             app.UseAuthentication();
 
